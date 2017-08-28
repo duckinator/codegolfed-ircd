4a580c557160092906769925a68e375a2bff4d4e
Ellen Marie Dash <me@duckie.co>
2017-08-27 20:50:25 -0400

    .

--- ircd.rb
+++ ircd.rb
@@ -6,7 +6,6 @@ x.each_request(true) {|c|
 n=nil
 cs[c] = nil
 c.each_line{|l|
-puts "[RECV #{c}] #{l}"
 on=nil
 if l=~/^NICK/
 on=n
@@ -17,13 +16,8 @@ s(c,":s 001 #{n} :x") if on.nil?
 end
 next if n.nil?
 next if l=~/PING/
-puts "??? #{n.inspect}"
-puts "?? cs: #{cs.inspect}"
 cx=cs.dup
-#cx-=c if l=~/PRIVMSG|NOTICE/
 cx=cx.reject{|z|z==c} if l=~/^PRIVMSG|^NOTICE/
-puts "cs: #{cs.inspect}"
-puts "cx: #{cx.inspect}"
 cx.each{|(y,_)|s(y,":#{on||n}!r@h #{l}")}
 c.close if l=~/^QUIT/
 }}
