b59096a6d59008ad764bc6f2346675eeba46b865
Ellen Marie Dash <me@duckie.co>
2017-08-27 21:33:14 -0400

    .

--- ircd.rb
+++ ircd.rb
@@ -2,20 +2,17 @@ require'net/socket'
 def s(x,l)x.puts l end
 x=Net::Socket::TCP::Server.new('0.0.0.0',6667)
 d={}
-x.each_request(!!1){|c|
-d[c]=n=nil
-c.each_line{|l|
-m=nil
+x.each_request(!!1){|c|d[c]=n=nil
+c.each_line{|l|m=nil
 if l=~/^NICK (.*)\r/
 m=n
 t=$1
 next s(c,":s 433 #{n||'*'} #{t} :") if d.any?{|(_,b)|b==t}
 d[c]=n=t
-m||s(c,":s 001 #{n} :x")
+m||s(c,":s 001 #{n} :")
 end
 n&&l=~/^PI/&&next
-cx=d.dup
-cx=cx.reject{|z|z==c} if l=~/^PR|^NO/
-cx.each{|(y,_)|s(y,":#{m||n}!r@h #{l}")}
+e=d.reject{|(z,_)|z==c&&l=~/^PR|^NO/}
+e.each{|(y,_)|s(y,":#{m||n}!r@h #{l}")}
 l[0]==?Q&&c.close}}
 loop{sleep 1}
