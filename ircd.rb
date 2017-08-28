f9d361319d85b5c4dca43840807860d02fb25e66
Ellen Marie Dash <me@duckie.co>
2017-08-27 22:37:48 -0400

    .

--- ircd.rb
+++ ircd.rb
@@ -5,10 +5,10 @@ loop{Thread.new(x.accept){|c|d[c]=n=nil
 c.each_line{|l|if l=~/^N.+ (.*)\r/
 m=n
 t=$1
-next c.puts":s 433 #{n||?*} #{t} :"if d.any?{|b|b[1]==t}
+next c.puts":s 433 #{n||?*} #{t} :"if d.any?{|(_,y)|y==t}
 d[c]=n=t
 m||c.puts(":s 001 #{n} :")end
-n&&l=~/^PI/&&next
-e=d.reject{|(z,_)|z==c&&l=~/^PR|^NO/}
-e.each{|b|b[0].puts":#{m||n}!u@h "+l}
+l=~/^PI/&&next
+e=d.reject{|(z,_)|z==c&&l=~/^PR/}
+e.map{|(z,_)|z.puts":#{m||n}!u@h "+l}
 l[0]==?Q&&c.close}}}
