5a7eebd7f456e4e06ca79d1e7aacd39072100b68
Ellen Marie Dash <me@duckie.co>
2017-08-27 21:00:14 -0400

    .

--- ircd.rb
+++ ircd.rb
@@ -7,7 +7,7 @@ n=nil
 d[c] = nil
 c.each_line{|l|
 on=nil
-if l=~/^NICK/
+if l=~/^NI/
 on=n
 tn=l.split(' ').last
 next s(c,":s 433 #{n||'*'} #{tn} :") if d.any?{|(_,b)|b==tn}
@@ -15,10 +15,10 @@ d[c]=n=tn
 s(c,":s 001 #{n} :x") if on.nil?
 end
 next if n.nil?
-next if l=~/PING/
+next if l=~/^PI/
 cx=d.dup
-cx=cx.reject{|z|z==c} if l=~/^PRIVMSG|^NOTICE/
+cx=cx.reject{|z|z==c} if l=~/^PR|^NO/
 cx.each{|(y,_)|s(y,":#{on||n}!r@h #{l}")}
-c.close if l=~/^QUIT/
+c.close if l=~/^Q/
 }}
 loop{sleep 1}
