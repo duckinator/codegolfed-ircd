eb5ab7da23bea5c9e1f3828222a384f755d2e928
Ellen Marie Dash <me@duckie.co>
2017-08-27 22:09:02 -0400

    .

--- ircd.rb
+++ ircd.rb
@@ -3,8 +3,7 @@ def s(x,l)x.puts l end
 x=TCPServer.new('0.0.0.0',6667)
 d={}
 loop{Thread.new(x.accept){|c|d[c]=n=nil
-c.each_line{|l|m=nil
-if l=~/^N.+ (.*)\r/
+c.each_line{|l|if l=~/^N.+ (.*)\r/
 m=n
 t=$1
 next s(c,":s 433 #{n||'*'} #{t} :")if d.any?{|b|b[1]==t}
