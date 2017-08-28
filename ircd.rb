b7022cd53910127dc003639adf55f9f7b694e085
Ellen Marie Dash <me@duckie.co>
2017-08-27 22:18:28 -0400

    .

--- ircd.rb
+++ ircd.rb
@@ -1,12 +1,12 @@
 require'socket'
 def s(x,l)x.puts l end
-x=TCPServer.new('0.0.0.0',6667)
+x=TCPServer.new'0.0.0.0',6667
 d={}
 loop{Thread.new(x.accept){|c|d[c]=n=nil
 c.each_line{|l|if l=~/^N.+ (.*)\r/
 m=n
 t=$1
-next s(c,":s 433 #{n||'*'} #{t} :")if d.any?{|b|b[1]==t}
+next s c,":s 433 #{n||'*'} #{t} :"if d.any?{|b|b[1]==t}
 d[c]=n=t
 m||s(c,":s 001 #{n} :")end
 n&&l=~/^PI/&&next
