ca133907e49005840c3540cb478822ed22a9823b
Ellen Marie Dash <me@duckie.co>
2017-08-27 22:46:18 -0400

    D:

--- ircd.rb
+++ ircd.rb
@@ -2,9 +2,9 @@ require'socket'
 x=TCPServer.new'0.0.0.0',6667
 d={}
 loop{Thread.new(x.accept){|c|d[c]=n=nil
-c.each_line{|l|if l=~/^N.+ (.*)\r/
+c.each_line{|l|if l=~/^N.+ (.*)/
 m=n
-t=$1
+t=$1.strip
 next c.puts":s 433 #{n||?*} #{t} :"if d.any?{|(_,y)|y==t}
 d[c]=n=t
 m||c.puts(":s 001 #{n} :")end
