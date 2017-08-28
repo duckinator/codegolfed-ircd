16b98e8c8c09eea4204b438eb6679175d3649bb9
Ellen Marie Dash <me@duckie.co>
2017-08-27 20:57:38 -0400

    .

--- ircd.rb
+++ ircd.rb
@@ -1,22 +1,22 @@
 require'net/socket'
 def s(x,l)x.puts l end
 x=Net::Socket::TCP::Server.new('0.0.0.0',6667)
-cs={}
-x.each_request(true) {|c|
+d={}
+x.each_request(!!1) {|c|
 n=nil
-cs[c] = nil
+d[c] = nil
 c.each_line{|l|
 on=nil
 if l=~/^NICK/
 on=n
 tn=l.split(' ').last
-next s(c,":s 433 #{n||'*'} #{tn} :") if cs.any?{|(_,b)|b==tn}
-cs[c]=n=tn
+next s(c,":s 433 #{n||'*'} #{tn} :") if d.any?{|(_,b)|b==tn}
+d[c]=n=tn
 s(c,":s 001 #{n} :x") if on.nil?
 end
 next if n.nil?
 next if l=~/PING/
-cx=cs.dup
+cx=d.dup
 cx=cx.reject{|z|z==c} if l=~/^PRIVMSG|^NOTICE/
 cx.each{|(y,_)|s(y,":#{on||n}!r@h #{l}")}
 c.close if l=~/^QUIT/
