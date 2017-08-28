e8bf919c3a042aadb5f9ac8a479c5e135a041a39
Ellen Marie Dash <me@duckie.co>
2017-08-27 21:05:05 -0400

    .

--- ircd.rb
+++ ircd.rb
@@ -6,19 +6,19 @@ x.each_request(!!1) {|c|
 n=nil
 d[c] = nil
 c.each_line{|l|
-on=nil
+m=nil
 if l=~/^NI/
-on=n
+m=n
 tn=l.split(' ').last
 next s(c,":s 433 #{n||'*'} #{tn} :") if d.any?{|(_,b)|b==tn}
 d[c]=n=tn
-s(c,":s 001 #{n} :x") if on.nil?
+m||s(c,":s 001 #{n} :x")
 end
-next if n.nil?
+next if !n
 next if l=~/^PI/
 cx=d.dup
 cx=cx.reject{|z|z==c} if l=~/^PR|^NO/
-cx.each{|(y,_)|s(y,":#{on||n}!r@h #{l}")}
+cx.each{|(y,_)|s(y,":#{m||n}!r@h #{l}")}
 c.close if l=~/^Q/
 }}
 loop{sleep 1}
